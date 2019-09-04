552852d5d342e7a3839575d07431167d7fd07668
life <2905033729@qq.com>
2019-09-04 19:23:51 +0800

    add the uboot for 4412 support DTS

---
+++ uboot_tiny4412/board/samsung/tiny4412/config.mk
@@ -0,0 +1,15 @@
+# 
+#  (C) Copyright 2011 Samsung Electronics Co. Ltd
+#  
+#  See file CREDITS for list of people who contributed to this
+#  project.
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License version 2 as
+#  published by the Free Software Foundation.
+#
+#
+#************ life 20190903 begin >>> [2/2] delete MMU
+#CONFIG_SYS_TEXT_BASE = 0xc3e00000
+CONFIG_SYS_TEXT_BASE = 0x43e00000
+#************ life 20190903 end >>> [2/2] delete MMU
---
+++ uboot_tiny4412/include/timestamp_autogenerated.h
@@ -0,0 +1,2 @@
+#define U_BOOT_DATE "Sep 04 2019"
+#define U_BOOT_TIME "00:19:43"
